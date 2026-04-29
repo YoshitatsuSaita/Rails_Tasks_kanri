f8bd86880f61ff9b2630bf3e7c0cec8b57baa9c5
YoshitatsuSaita <ysaita1215@gmail.com>
2026-04-30 05:06:15 +0900

    新規作成ページの制作

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :set_current_user
+  
+  def set_current_user
+    @user = current_user
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,25 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = @user.tasks
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = @user.tasks.build(user_params)
+    if @task.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to user_tasks_path(@user)
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:task).permit(:name, :detail)
   end
 end
